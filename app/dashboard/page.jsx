899bdad0a5f4afec7600416a1c8849c77b1becee
ConorCasey99 <conorcasey258@gmail.com>
2024-08-28 16:17:57 +0100

    Home Page Displaying Artists UI Skeleton

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,13 @@
 export default async function DashboardPage () {
 
+    
+
+
+
+
+
+
+
     return (
         <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto">
